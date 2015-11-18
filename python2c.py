f8c4f74061cd7935382e39ee0335ba6da2164b65
Leo is awesome <lchan1994@yahoo.com>
2015-11-17 21:53:17 -0500

    removed unecessary file

--- python2c.py
+++ python2c.py
@@ -10,7 +10,38 @@ import subprocess
 
 from blocks import *
 
-from arg_positions import prettyparseprint
+
+def prettyparseprintfile(filename, spaces=4):
+    with open(filename, "r") as f:
+        prettyparseprint(f.read(), spaces)
+
+
+def prettyparseprint(code, spaces=4):
+    node = ast.parse(code)
+    text = ast.dump(node)
+    indent_count = 0
+    i = 0
+    while i < len(text):
+        c = text[i]
+
+        if text[i:i+2] in ("()", "[]"):
+            i += 1
+        elif c in "([":
+            indent_count += 1
+            indentation = spaces*indent_count
+            text = text[:i+1] + "\n" + " "*indentation + text[i+1:]
+        elif c in ")]":
+            indent_count -= 1
+            indentation = spaces*indent_count
+            text = text[:i] + "\n" + " "*indentation + text[i:]
+            i += 1 + indentation
+
+            if text[i:i+3] in ("), ", "], "):
+                text = text[:i+2] + "\n" + " "*indentation + text[i+3:]
+                i += indentation
+
+        i += 1
+    print(text)
 
 
 def includes_from_code(code):
